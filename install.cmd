c295a96428ea65bf8cd171d39a124e255781029f
fiziechemi <30676109+fiziechemi@users.noreply.github.com>
2022-09-26 14:07:32 +0800

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -1,1 +1,40 @@
-echo hehe
+@echo off
+reg query "HKLM\Software\Microsoft\Command Processor" /v AutoRun >nul 2>nul
+if %ERRORLEVEL% EQU 0 (
+    goto editReg
+) else (
+    goto addReg
+)
+
+:editReg
+
+for /f "tokens=3" %%a in ('reg query "HKLM\Software\Microsoft\Command Processor"  /V AutoRun  ^|findstr /ri "REG_EXPAND_SZ"') do set regValue=%%a
+reg delete "HKLM\Software\Microsoft\Command Processor" /v AutoRun /f >nul 2>nul
+if not %ERRORLEVEL% EQU 0 (
+    echo ERROR: Access is denied. Please execute code as an administrator.
+    goto end
+)
+reg add "HKLM\Software\Microsoft\Command Processor" /v AutoRun /t REG_EXPAND_SZ /d "%%USERPROFILE%%\.pywin-venv\init.bat" /f >nul 2>nul
+if not %ERRORLEVEL% EQU 0 (
+    echo ERROR: Access is denied. Please execute code as an administrator.
+) else (
+    echo The installation completed successfully.
+    if "%regValue%"=="%%USERPROFILE%%\.pywin-venv\init.bat" (
+        echo AutoRun registry changed in "HKLM\Software\Microsoft\Command Processor".
+        echo               [Name]  [Type]        [Data]
+        echo Old registry: Autorun REG_EXPAND_SZ "%regValue%"
+        echo New registry: Autorun REG_EXPAND_SZ "%%USERPROFILE%%\.pywin-venv\init.bat"
+    )
+)
+goto end
+
+:addReg
+
+reg add "HKLM\Software\Microsoft\Command Processor" /v AutoRun /t REG_EXPAND_SZ /d %%USERPROFILE%%\.pywin-venv\init.bat /f >nul 2>nul
+if not %ERRORLEVEL% EQU 0 (
+    echo ERROR: Access is denied. Please execute code as an administrator.
+) else (
+    echo The installation completed successfully.
+)
+
+:end
